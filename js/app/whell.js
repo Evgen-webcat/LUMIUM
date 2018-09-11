ed533b898418a9bf886bd8154078ff6b321d1163
Evgen-webcat <evgen@webcat.by>
2018-09-11 22:39:59 +0300

    Added new animation.

--- js/app/whell.js
+++ js/app/whell.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
 
     var currentPage = 0;
     var page = $('.page');
+    var vivus;
 
     function bindWheel() {
         $(window).bind('mousewheel', function (event) {
@@ -23,7 +24,7 @@ $(document).ready(function () {
 
     function togglePage(direction) {
         $(window).unbind('mousewheel');
-        page.eq(currentPage).fadeOut(500);
+        page.eq(currentPage).removeClass('active_page');
         if (direction == 'next') {
             currentPage++;
         } else if (direction == 'prev') {
@@ -31,12 +32,9 @@ $(document).ready(function () {
         }
         setTimeout(
             function () {
-                page.eq(currentPage).fadeIn();
+                page.eq(currentPage).addClass('active_page');
             },
             500);
-        setTimeout(function () {
-            bindWheel();
-        }, 1000);
         if (currentPage == 4) {
             setTimeout(function () {
                 $('.mouse').addClass('mouse-white');
@@ -44,6 +42,14 @@ $(document).ready(function () {
         } else {
             $('.mouse').removeClass('mouse-white');
         }
+        if (currentPage == 3) {
+            pageAnimation();
+        } else {
+
+        }
+        setTimeout(function () {
+            bindWheel();
+        }, 1000);
     }
 
     function nextPage() {
@@ -56,5 +62,35 @@ $(document).ready(function () {
         if (currentPage > 0) {
             togglePage('prev');
         }
+    };
+
+    function pageAnimation() {
+        setTimeout(function () {
+            $('.dot').css({
+                'width': '100px',
+                'height': '100px',
+                'background-color': '#4c4c4c'
+            });
+            $('.logo_img').css({
+                'width': '100px',
+                'height': '100px',
+                'box-shadow': '0 0 25px rgba(255, 255, 255, .5)'
+            });
+        }, 1000);
+        setTimeout(function () {
+            $('#diagramm').css('display', 'block');
+            vivus = new Vivus('diagramm', {
+                duration: 200,
+                type: 'sync'
+            })
+            vivus.play();
+        }, 2500);
+        setTimeout(function () {
+            $("#diagramm").addClass('diagramm_gradient');
+            var diagramm = document.getElementById('g7390');
+            var logo_block = document.getElementById('logo_block');
+            var parallax_img = new Parallax(diagramm);
+            var parallax_img = new Parallax(logo_block);
+        }, 6000);
     };
 });
