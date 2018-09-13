a87e82190a7c4d0cf46443b882ad55c32ab9e001
evgen-webcat <evgen@webcat.by>
2018-09-13 17:27:38 +0300

    Added product page.

--- js/app/mouse_parallax.js
+++ js/app/mouse_parallax.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     var img = document.getElementById('parallax_block');
+    if (img) {
     var parallax_img = new Parallax(img);
+}
 });
---
+++ js/app/range.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('input[type="range"]').rangeslider({
+        rangeClass: 'rangeslider',
+        disabledClass: 'rangeslider--disabled',
+        horizontalClass: 'rangeslider--horizontal',
+        verticalClass: 'rangeslider--vertical',
+        fillClass: 'rangeslider__fill',
+        handleClass: 'rangeslider__handle'
+    });
+});
